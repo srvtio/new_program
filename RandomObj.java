4012ba54c46c794681b98235748ff115d40ff046
srvtio <srvtio@yahoo.co,jp>
2016-07-28 18:16:52 +0900

    多孔質物体をランダムに配置するプログラム完成版(2016.7.28)
    物体をランダムに配置し，以下のファイルを作成
    
    Parameter().dat
    KnudsenData().dat
    porous_conf.gp
    
    gnuplotファイルはそのまま実行可能

--- RandomObj.java
+++ RandomObj.java
@@ -1,41 +1,44 @@
-// 最新版 2016.7.27
-import java.io.File;
-import java.io.FileWriter;
-import java.io.BufferedWriter;
-import java.io.PrintWriter;
-import java.io.IOException;
+/*--- 最新版 2016.7.28 ---*/
+// import java.io.File ;
+// import java.io.FileWriter ;
+// import java.io.BufferedWriter ;
+// import java.io.PrintWriter ;
+// import java.io.IOException ;
+import java.io.*;
 
 public class RandomObj {
 
     public static void main(String[] args) {
+	int incx,incy;
 	int iCellNumber1 = 32 ;
 	int iCellNumber2 = 32 ;
-
-	double Knudsen1 = 0.0 ;
-	double Knudsen2 = 100.0 ;
-
-	int seed = 36 ;
-
-	int incx,incy ;
-	int Number = 0 ;
+	int iTotalCell   = iCellNumber1 * iCellNumber2 ;
+	int seed         = 1 ;
+	int itemp        = 0 ;
+	int Number       = 0 ;
+	int KnudsenPt    = 1 ;
+	int iParallel    = 7 ;
+	int iPattern     = 1011660 ;
+	double porosity      = 0.0 ;
+	double Knudsen1      = 0.0 ;
+	double Knudsen2      = 100.0 ;
+	double dtemp         = 0.0 ;
+	double FinalPorosity = 0.50 ;
 
 	int NumberCell[][] = new int[iCellNumber1][iCellNumber2] ;
 	double Knudsen[][] = new double[iCellNumber1][iCellNumber2] ;
 
-	int itemp = 0 ;
-	double dtemp = 0.0 ;
-
-	double porosity = 0.0 ;
-	double FinalPorosity = 0.62 ;
-
+	/*--- Knudsen[][]の初期値設定 ---*/
 	for(incx=0 ; incx<iCellNumber1 ; incx++){
 	    for(incy=0 ; incy<iCellNumber2 ; incy++){
 		Knudsen[incx][incy] = Knudsen2 ;
 	    }
 	}
 
-	for(int i=0 ; i<1000 ; i++){
-	    // 空隙部分のセルに通し番号をつける
+	/*--- 空隙部分をKnudsen1で塗りつぶしていく繰り返し計算 ---*/
+	for(int i=0 ; i<iTotalCell ; i++){
+
+	    /*--- 空隙部分のセルに通し番号をつける ---*/
 	    Number = 0 ;
 
 	    for(incx=0 ; incx<iCellNumber1 ; incx++){
@@ -49,7 +52,7 @@ public class RandomObj {
 		}
 	    }
 
-	    // 空隙部分の中からランダムにセルを選んで，その部分のKnusenをKnudsen1にする
+	    /*--- 空隙部分の中からランダムにセルを選んで，その部分のKnusenをKnudsen1にする ---*/
 	    for(incx=0 ; incx<iCellNumber1 ; incx++){
 		for(incy=0 ; incy<iCellNumber2 ; incy++){
 		
@@ -69,45 +72,100 @@ public class RandomObj {
 	}
 
 	System.out.println(porosity) ;
-	
-	// ファイルへの書き込み
+
+	// /*--- ファイルの読み取り ---*/
+	// double a[] = new double[1000] ;
+	// int i = 0 ;
+	// try{
+	//     File file = new File("Parameter(CN1_"+iCellNumber1+",CN2_"+iCellNumber2+").dat") ;
+	//     FileReader fr = new FileReader(file) ; 
+	//     StreamTokenizer st = new StreamTokenizer(fr) ; 
+
+	//     // nextToken()で次の行を読み取る
+	//     st.nextToken() ;
+	//     KnudsenPt = (int)st.nval ;
+	//     st.nextToken() ;
+	//     iParallel = (int)st.nval ;
+	//     st.nextToken() ;
+	//     iPattern = (int)st.nval ;
+	//     st.nextToken() ;
+	//     porosity = st.nval ;
+
+	//     fr.close() ;
+
+	// }catch(Exception e){
+	//     System.out.println(e) ;
+	// }
+
+	// System.out.println(KnudsenPt) ; 
+	// System.out.println(iParallel) ; 
+	// System.out.println(iPattern) ; 
+	// System.out.println(porosity) ; 
+
+	/*--- ファイルへの書き込み ---*/
 	try{
-	    File file = new File("KnudsenData(CN1_32,CN2_32,pt1544).dat");
+	    File file1 = new File("Parameter(CN1_"+iCellNumber1+",CN2_"+iCellNumber2+").dat") ;
+	    File file2
+		= new File("KnudsenData(CN1_"+iCellNumber1+",CN2_"+iCellNumber2+",pt"+iPattern+").dat") ;
+	    File file3 = new File("porous_conf.gp") ;
 
-	    if (checkBeforeWritefile(file)){
-		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(file)));
+	    PrintWriter pw1 = new PrintWriter(new BufferedWriter(new FileWriter(file1))) ;
+	    PrintWriter pw2 = new PrintWriter(new BufferedWriter(new FileWriter(file2))) ;
+	    PrintWriter pw3 = new PrintWriter(new BufferedWriter(new FileWriter(file3))) ;
 
-		for(incx=0 ; incx<iCellNumber1 ; incx++){
-		    for(incy=0 ; incy<iCellNumber2 ; incy++){
-			pw.printf("%8d", incx) ;
-			pw.printf("%8d", incy) ;
-			pw.printf("%16f %n", Knudsen[incx][incy]) ;			
-		    }
-		    pw.printf("%8s %n", "  ") ;
+	    //パラメータファイルの作成
+	    pw1.printf("%2d %n", KnudsenPt) ;
+	    pw1.printf("%2d %n", iParallel) ;
+	    pw1.printf("%8d %n", iPattern) ;
+	    pw1.printf("%8f %n", porosity) ;
+
+	    //多孔質配置のデータファイル作成
+	    for(incx=0 ; incx<iCellNumber1 ; incx++){
+		for(incy=0 ; incy<iCellNumber2 ; incy++){
+		    pw2.printf("%8d", incx) ;
+		    pw2.printf("%8d", incy) ;
+		    pw2.printf("%16f %n", Knudsen[incx][incy]) ;			
 		}
-
-		pw.close();
-
-	    }else{
-		System.out.println("ファイルに書き込めません");
+		pw2.printf("%8s %n", "  ") ;
 	    }
+
+	    //gnuplot作成ファイルの作成
+	    pw3.printf("set terminal eps %n") ;
+	    pw3.printf("set pm3d map %n") ;
+	    pw3.printf("set pm3d corners2color c2 %n") ;
+	    pw3.printf("set size ratio 1 %n") ;
+	    pw3.printf("set output '"+iPattern+".eps' %n") ;
+	    pw3.printf("unset colorbox %n") ;
+	    pw3.printf("set palette rgbformulae 22,13,-31 %n") ;
+	    pw3.printf("set xrange [0:31] %n") ;
+	    pw3.printf("set yrange [0:31] %n") ;
+	    pw3.printf("set xlabel 'x1' %n") ;
+	    pw3.printf("set ylabel 'x2' %n") ;
+	    pw3.printf("splot 'KnudsenData(CN1_"+iCellNumber1+",CN2_"+iCellNumber2+",pt"+iPattern+").dat'u 1:2:3  palette notitle %n") ;
+	    pw3.printf("unset output %n") ;
+	    pw3.printf("reset %n") ;
+
+	    pw1.close() ;
+	    pw2.close() ;
+	    pw3.close() ;
+
 	}catch(IOException e){
-	    System.out.println(e);
+	    System.out.println(e) ;
 	}
     }
 
-    // ファイルがすでに存在するか，普通のファイル，書き込み可能かを判断するメソッド
-    private static boolean checkBeforeWritefile(File file){
-	if (file.exists()){
-	    if (file.isFile() && file.canWrite()){
-		return true;
-	    }
-	}
+    // /*--- ファイルがすでに存在するか，普通のファイル，書き込み可能かを判断するメソッド ---*/
+    // private static boolean checkBeforeWritefile(File file){
+    // 	if (file.exists()){
+    // 	    if (file.isFile() && file.canWrite()){
+    // 		return true ;
+    // 	    }
+    // 	}
 
-	return false;
-    }
+    // 	return false ;
+    // }
 
-    // 乱数生成用メソッド
+    /*--- 乱数生成用メソッド ---*/
     static int ma[] = new int[56] ;
     static int inext  = 0 ;
     static int inextp = 0 ;
